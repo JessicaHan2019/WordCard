4a645ed26738a00d70c4f238678d77d98bd5a3d3
Jialun Han <hanjialunjessica@gmail.com>
2020-06-18 10:50:45 +0930

    Initial commit

---
+++ src/main/java/com/jessica/wordcard/WordcardApplication.java
@@ -0,0 +1,13 @@
+package com.jessica.wordcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordcardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordcardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jessica/wordcard/WordcardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jessica.wordcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordcardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
